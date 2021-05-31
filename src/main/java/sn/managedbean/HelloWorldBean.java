5ad5f82421cbfa9be6d6d48672179de6d543808c
Mamadou Seydou Diallo <diallodiery301@gmail.com>
2021-05-31 23:36:25 +0000

    boostrap project

---
+++ src/main/java/sn/managedbean/HelloWorldBean.java
@@ -0,0 +1,16 @@
+package sn.managedbean;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+@Named("helloworld")
+@ViewScoped
+public class HelloWorldBean implements Serializable {
+
+   public String getMessage() {
+      return "Hello World from Fuertefentura";
+   }
+
+}
